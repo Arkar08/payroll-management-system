128f43d7c2dfb2f4fe2b8eab90c46af0bc29b100
uiux@okdollar frontend <uiux@okdollar.com>
2025-02-08 16:05:07 +0630

    adding: logout page add

--- src/pages/auth/logout/Logout.jsx
+++ src/pages/auth/logout/Logout.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import { Button } from 'antd'
+import 'react'
 
 const Logout = () => {
   return (
-    <div>
-        logout
+    <div className='logoutContainer'>
+        <h4 className='logoutText'>Are You Want To Logout?</h4>
+        <div className='btnGroup'>
+            <Button className='noBtn'>No</Button>
+            <Button className='yesBtn'>Yes</Button>
+        </div>
     </div>
   )
 }
