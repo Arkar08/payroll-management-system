e97e1c4cec1c6f09101a41e346274c7efdb59239
uiux@okdollar frontend <uiux@okdollar.com>
2025-02-10 10:36:39 +0630

    login design

--- src/pages/auth/login/Login.jsx
+++ src/pages/auth/login/Login.jsx
@@ -1,10 +1,60 @@
-import React from 'react'
+import 'react'
+import { Button, Checkbox, Form, Input } from 'antd';
+import { useNavigate } from 'react-router-dom';
+
+
 
 const Login = () => {
+  
+  const navigate = useNavigate()
+
+  const onFinish = () =>{
+    navigate('/dashboard')
+  }
+
   return (
-    <div>
-        Login
+  <div className='loginContainer'>
+    <div className="loginForm">
+      <Form onFinish={onFinish}>
+        <p className='loginText'>Login</p>
+        <Form.Item
+          label="Username"
+          name="username"
+          rules={[
+            {
+              required: true,
+              message: 'Please input your username!',
+            },
+          ]}
+        >
+          <Input autoComplete='off'/>
+        </Form.Item>
+
+        <Form.Item
+          label="Password"
+          name="password"
+          rules={[
+            {
+              required: true,
+              message: 'Please input your password!',
+            },
+          ]}
+        >
+          <Input.Password />
+        </Form.Item>
+
+        <Form.Item name="remember" valuePropName="checked" label={null} className='remember'>
+          <Checkbox>Remember me</Checkbox>
+        </Form.Item>
+
+        <Form.Item label={null} className='buttonSubmit'>
+          <Button type="primary" htmlType="submit" className='loginButton'>
+            Login
+          </Button>
+        </Form.Item>
+      </Form>
     </div>
+  </div>
   )
 }
 
--- src/pages/auth/logout/Logout.jsx
+++ src/pages/auth/logout/Logout.jsx
@@ -1,13 +1,21 @@
 import { Button } from 'antd'
 import 'react'
+import { useNavigate } from 'react-router-dom'
 
 const Logout = () => {
+
+  const navigate = useNavigate()
+
+  const logout = () =>{
+    navigate('/auth/login')
+  }
+  
   return (
     <div className='logoutContainer'>
         <h4 className='logoutText'>Are You Want To Logout?</h4>
         <div className='btnGroup'>
             <Button className='noBtn'>No</Button>
-            <Button className='yesBtn'>Yes</Button>
+            <Button className='yesBtn' onClick={()=>logout()}>Yes</Button>
         </div>
     </div>
   )
